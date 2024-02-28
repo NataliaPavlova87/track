24306902242512d65f16c3a398117582ec5cadb0
Natalia Pavlova <natalia.pavlova@imedia.ru>
2024-02-28 20:40:35 +0300

    Добавление конфигов

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './src/**/*.{js,jsx,ts,tsx,vue}',
+        './pages/**/*.{js,jsx,ts,tsx,vue}',
+        './layouts/**/*.{js,jsx,ts,tsx,vue}'
+    ],
+    theme: {
+        extend: {}
+    },
+    plugins: []
+}
